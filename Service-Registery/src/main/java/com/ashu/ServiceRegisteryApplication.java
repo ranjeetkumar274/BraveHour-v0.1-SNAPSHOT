fafeb1b591419095e31ef35439e28350518f6c86
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-21 13:30:54 +0530

    Testing of Platform Done

--- Service-Registery/src/main/java/com/ashu/ServiceRegisteryApplication.java
+++ Service-Registery/src/main/java/com/ashu/ServiceRegisteryApplication.java
@@ -2,14 +2,15 @@ package com.ashu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaServer
+
+public class ServiceRegisteryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
 	}
 
 }
---
+++ Service-Registery/src/main/java/com/ashu/rest/DemoRest.java
@@ -0,0 +1,17 @@
+package com.ashu.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRest {
+	
+	@Value("${msg}")
+	private String message;
+	
+	@GetMapping("/test")
+	public String demo() {
+		return message;
+	}
+}
